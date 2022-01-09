f4871df9b732c025ac128f8016fb867546401b63
Arsal Fadilah <arsal.fadilah.tif420@polban.ac.id>
2022-01-09 08:52:06 +0700

    finally (end of support)

--- ecomm/views.py
+++ ecomm/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from ecomm.models import product
+from ecomm.models import keranjang
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-    datamap = {
-        'data' : 10000,
-        'kalimat' : "PROGANTARA"
-    }
-    return render(request, 'index.html', datamap)
+    data_product = product.objects.all()
+    data_keranjang = keranjang.objects.all()
+    return render(request, 'index.html', {'products': data_product, 'keranjang':data_keranjang})
+
+def add(request):
+    if request.method == 'POST':
+        data = keranjang(id_produk=request.POST.get('id_product'), jmlh=1)
+        data.save()
+    
+    return redirect('/')    
+
+def delete(request):
+    if request.method == 'POST':
+        data = keranjang.objects.get(id_produk=request.POST.get('id_product'))
+        data.delete()
+    
+    return redirect('/')
--- myEcommerce/settings.py
+++ myEcommerce/settings.py
@@ -78,10 +78,17 @@ WSGI_APPLICATION = 'myEcommerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'djangodb',  
+        'USER': 'root',  
+        'PASSWORD': '',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 
